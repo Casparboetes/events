aab5271534f3c6c68f74dccb26d377c3576bc3a1
CASPARBOETES <CASPARBOETES@GMAIL.COM>
2018-01-31 14:09:31 +0100

    Add Tests, add subTitle component, add compontstylesheet

--- src/App.js
+++ src/App.js
@@ -2,23 +2,16 @@ import React, { Component } from 'react'
 import logo from './logo.svg'
 import './App.css'
 import Title from './components/Title'
+import SubTitle from './components/SubTitle'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <Title content="Super Secret Events" />
+        <SubTitle content="Plan your own Secret" />
         </header>
-        <p className="App-intro">
-        <Title content="Super Secret Events" />
-        <Title content="Plan your own Secret" />
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-
-        <Title content="Super Secret Events" />
-        <Title content="Plan your own Secret" />
 
       </div>
     );
--- src/components/SubTitle.js
+++ src/components/SubTitle.js
@@ -2,16 +2,16 @@ import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 import './ComponentStyler.css'
 
-class Title extends PureComponent {
+class subTitle extends PureComponent {
   static propTypes = {
     content: PropTypes.string.isRequired,
   }
 
   render() {
     return(
-      <h1 className="Title">{ this.props.content }</h1>
+      <h2 className="subTitle">{ this.props.content }</h2>
     )
   }
 }
 
-export default Title
+export default subTitle
