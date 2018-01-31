9f099a8caeee61bdb890704e05cdaf8ba2070221
CASPARBOETES <CASPARBOETES@GMAIL.COM>
2018-01-31 14:21:24 +0100

    Add test for subtitle, jeee

--- src/App.test.js
+++ src/App.test.js
@@ -3,10 +3,12 @@ import { shallow } from 'enzyme'
 import ReactDOM from 'react-dom'
 import App from './App'
 import Title from './components/Title'
+import SubTitle from './components/SubTitle'
 
 describe('<App />', () => {
   const app = shallow(<App />)
   const title = <Title content="Super Secret Events" />
+  const subtitle = <SubTitle content="Plan your own Secret" />
 
   it('wraps everything in a div tag', () => {
     expect(app).toHaveTagName('div')
@@ -16,6 +18,9 @@ describe('<App />', () => {
     expect(app).toContainReact(title)
   })
 
+  it('contains a SubTitle', () => {
+    expect(app).toContainReact(subtitle)
+  })
 
     it('renders without crashing', () => {
       const div = document.createElement('div')
--- src/components/SubTitle.js
+++ src/components/SubTitle.js
@@ -2,16 +2,16 @@ import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 import './ComponentStyler.css'
 
-class subTitle extends PureComponent {
+class SubTitle extends PureComponent {
   static propTypes = {
     content: PropTypes.string.isRequired,
   }
 
   render() {
     return(
-      <h2 className="subTitle">{ this.props.content }</h2>
+      <h2 className="SubTitle">{ this.props.content }</h2>
     )
   }
 }
 
-export default subTitle
+export default SubTitle
